debb69b972fb8561d8fbdaeb5e82633ed487454a
Jose Arturo Herrera Rivera <joherrerar@unal.edu.co>
2025-03-01 19:02:06 -0500

    Eliminar usuario listo

--- src/components/EditUserForm/index.tsx
+++ src/components/EditUserForm/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import ButtonAction from "../ButtonAction";
 import styles from "./style.module.css";
+import { get } from "axios";
+import api from "../../api/axiosConfig";
+import Button from "../Button";
 
 type Props = {};
 
@@ -23,33 +26,50 @@ function EditUserForm({}: Props) {
     const formData = new FormData();
     formData.append("image", file); // "image" debe coincidir con el nombre que espera el backend
 
-    /*
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/upload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-      console.log("Imagen subida con éxito:", response.data);
-    } catch (error) {
-      console.error("Error al subir la imagen:", error);
-    }*/
-
     console.log(file);
     console.log("Formulario enviado");
   };
 
-  const handleSubmitName = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Formulario enviado");
+  const handleDelete = async () => {
+    try {
+      const response = await api.delete("/eliminarperfil", {
+        headers: { Authorization: sessionStorage.getItem("jwtToken") },
+      });
+      alert("Usuario eliminado");
+      sessionStorage.clear();
+      window.location.href = "/home";
+    } catch (error) {
+      console.log("Error al eliminar usuario", error);
+    }
   };
-  const handleSubmitPass = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmitPass = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Formulario enviado");
+
+    const formData = new FormData(e.currentTarget);
+
+    const verify = {
+      password: formData.get("password"),
+      password2: formData.get("password2"),
+    };
+
+    console.log(verify);
+
+    const payload = {
+      token: sessionStorage.getItem("jwtToken"),
+      newPassword: verify.password,
+    };
+
+    console.log(payload);
+
+    if (verify.password == verify.password2) {
+      try {
+        await api.post("/update-password", payload);
+        alert("Cambio de contraseña exitoso");
+      } catch (error) {
+        console.log("Error en el cambio de contraseña", error);
+      }
+    }
   };
   const handleSubmitPhoto = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -66,31 +86,6 @@ function EditUserForm({}: Props) {
   return (
     <>
       <div>
-        <h3 className={`${styles.title}`}>Información Personal</h3>
-        <hr />
-        <form onSubmit={handleSubmitName}>
-          <div className="container text-center">
-            <div className="row justify-content-between">
-              <div className="col-4">
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    className={`form-control ${styles.input}`}
-                    id="username"
-                    aria-describedby="username"
-                    placeholder="Nuevo nombre de usuario"
-                  />
-                </div>
-              </div>
-              <div className="col-4">
-                <ButtonAction text="Cambiar" type="submit" />
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-
-      <div>
         <h3 className={`${styles.title}`}>Contraseña</h3>
         <hr />
 
@@ -100,29 +95,22 @@ function EditUserForm({}: Props) {
               <div className="col-4">
                 <div className="mb-3">
                   <input
+                    required
+                    name="password"
                     type="password"
-                    className={`form-control ${styles.input}`}
-                    id="actualpassword"
-                    aria-describedby="actualpassword"
-                    placeholder="Contraseña Actual"
+                    className={`form-control ${styles.loginSlot}`}
+                    id="passwordSlotLogin"
+                    placeholder="Contraseña"
                   />
                 </div>
                 <div className="mb-3">
                   <input
+                    required
+                    name="password2"
                     type="password"
-                    className={`form-control ${styles.input}`}
-                    id="newpassword"
-                    aria-describedby="newpassword"
-                    placeholder="Contraseña Actual"
-                  />
-                </div>
-                <div className="mb-3">
-                  <input
-                    type="password"
-                    className={`form-control ${styles.input}`}
-                    id="password"
-                    aria-describedby="password"
-                    placeholder="Contraseña Actual"
+                    className={`form-control ${styles.loginSlot}`}
+                    id="passwordSlotLogin2"
+                    placeholder="Repetir Contraseña"
                   />
                 </div>
               </div>
@@ -146,7 +134,7 @@ function EditUserForm({}: Props) {
                   <input
                     type="file"
                     name="myImage"
-                    accept="image/png, image/gif, image/jpeg"
+                    accept="image/png"
                     onChange={handleFileChange}
                   />
                 </div>
@@ -157,6 +145,10 @@ function EditUserForm({}: Props) {
             </div>
           </div>
         </form>
+      </div>
+
+      <div className="container text-center my-5">
+        <Button text="Eliminar cuenta" onClick={handleDelete} />
       </div>
     </>
   );
